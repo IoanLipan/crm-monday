8341cf318e3bdfced52f26a7b71de32287e1996c
Lipan Ioan <lipanioan420@gmail.com>
2023-11-20 23:23:04 +0200

    cleanup server file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const PORT = 8000;
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -8,41 +7,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const url = process.env.URL;
-const token = process.env.ASTRA_DB_APPLICATION_TOKEN;
+const astraUrl = process.env.ASTRA_DB_URL;
+const astraToken = process.env.ASTRA_DB_APPLICATION_TOKEN;
 
 app.get("/tickets", async (req, res) => {
   const options = {
     method: "GET",
     headers: {
       Accept: "application/json",
-      "X-Cassandra-Token": token,
+      "X-Cassandra-Token": astraToken,
     },
   };
   try {
-    const response = await axios(`${url}?page-size=20`, options);
+    const response = await axios(`${astraUrl}?page-size=20`, options);
     res.status(200).json(response.data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
   }
 });
 
 app.get("/tickets/:documentId", async (req, res) => {
-  const id = req.params.documentId;
+  const documentId = req.params.documentId;
   const options = {
     method: "GET",
     headers: {
       Accept: "application/json",
-      "X-Cassandra-Token": token,
+      "X-Cassandra-Token": astraToken,
     },
   };
   try {
-    const response = await axios(`${url}/${id}`, options);
+    const response = await axios(`${astraUrl}/${documentId}`, options);
     res.status(200).json(response.data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
   }
 });
 
@@ -53,14 +52,14 @@ app.post("/tickets", async (req, res) => {
     method: "POST",
     headers: {
       Accept: "application/json",
-      "X-Cassandra-Token": token,
+      "X-Cassandra-Token": astraToken,
       "Content-Type": "application/json",
     },
     data: formData,
   };
 
   try {
-    const response = await axios(url, options);
+    const response = await axios(astraUrl, options);
     res.status(200).json(response.data);
   } catch (error) {
     console.error(error);
@@ -69,43 +68,44 @@ app.post("/tickets", async (req, res) => {
 });
 
 app.put("/tickets/:documentId", async (req, res) => {
-  const id = req.params.documentId;
+  const documentId = req.params.documentId;
   const data = req.body.data;
   const options = {
     method: "PUT",
     headers: {
       Accept: "application/json",
-      "X-Cassandra-Token": token,
+      "X-Cassandra-Token": astraToken,
     },
     data,
   };
   try {
-    const response = await axios(`${url}/${id}`, options);
+    const response = await axios(`${astraUrl}/${documentId}`, options);
     res.status(200).json(response.data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
   }
 });
 
 app.delete("/tickets/:documentId", async (req, res) => {
-  const id = req.params.documentId;
+  const documentId = req.params.documentId;
   const options = {
     method: "DELETE",
     headers: {
       Accept: "application/json",
-      "X-Cassandra-Token": token,
+      "X-Cassandra-Token": astraToken,
     },
   };
   try {
-    const response = await axios(`${url}/${id}`, options);
+    const response = await axios(`${astraUrl}/${documentId}`, options);
     res.status(200).json(response.data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
   }
 });
 
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
